50d0cda02c10166c73504ba090b299de82036b95
Thomas Kendall <thomas.j.kendall@gmail.com>
2015-06-29 21:12:42 -0500

    Wired up the 404 page.

---
+++ Homesteader.Web/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homesteader.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
